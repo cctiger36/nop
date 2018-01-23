476f81d5631b7eb241a7b0d901ae06df48bf0914
Weihu Chen <cctiger36@Weihu-no-iMac.local>
2018-01-23 23:18:56 +0900

    setup

--- contracts/NopToken.sol
+++ contracts/NopToken.sol
@@ -7,10 +7,4 @@ contract NOPToken is MintableToken {
   string public constant name = "NOP Token";
   string public constant symbol = "NOP";
   uint8 public constant decimals = 18;
-
-  uint256 public constant INITIAL_SUPPLY = 200000 * (10 ** uint256(decimals));
-
-  function NOPToken() public {
-    totalSupply_ = INITIAL_SUPPLY;
-  }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,10 +3,12 @@ const NOP = artifacts.require("./NOP.sol");
 const now = parseInt(new Date() / 1000);
 const startTime = now;
 const endTime = startTime + 3600;
-const rate = 1;
-const cap = 10000;
+const totalSupply = 20000000 * 10 ** 18;
+const weiLimit = 200 * 10 ** 18;
+const rate = totalSupply / weiLimit;
+// TODO: use multi-sig wallet
 const wallet = 0x8a20a13b75d0aefb995c0626f22df0d98031a4b6;
 
 module.exports = async function (deployer, network, accounts) {
-  deployer.deploy(NOP, startTime, endTime, rate, cap, wallet);
+  deployer.deploy(NOP, startTime, endTime, rate, totalSupply, wallet);
 }
