b7e1245ab163b81d1650965a829052a5d53d55d0
weihu_chen <cctiger36@gmail.com>
2018-01-28 16:31:28 +0900

    minor change

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -7,7 +7,7 @@ const totalSupply = 20000000 * 10 ** 18;
 const capOfWei = 200 * 10 ** 18;
 const rate = totalSupply / capOfWei;
 // TODO: use multi-sig wallet
-const wallet = 0x7BFFadB78B16E55c409550a8Ad6aE1aceA0f2d0B;
+const wallet = '0x7BFFadB78B16E55c409550a8Ad6aE1aceA0f2d0B';
 
 module.exports = async function (deployer, network, accounts) {
   deployer.deploy(NOP, startTime, endTime, rate, capOfWei, wallet);
